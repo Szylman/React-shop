65459f1e406051ecd7d10a080c8505758ba6a397
Szylman96 <szymek2255@gmail.com>
2022-07-06 17:03:40 +0200

    inicial styles and components

---
+++ src/components/Product/Product.js
@@ -0,0 +1,56 @@
+import styles from './Product.module.scss';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+
+const Product = props => {
+  Product.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    basePrice: PropTypes.number.isRequired,
+    color: PropTypes.array.isRequired,
+    sizes: PropTypes.array.isRequired
+  }
+
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img 
+          className={styles.image}
+          alt="Kodilla shirt"
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {props.basePrice}$</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              <li><button type="button" className={styles.active}>S</button></li>
+              <li><button type="button">M</button></li>
+              <li><button type="button">L</button></li>
+              <li><button type="button">XL</button></li>
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
+              <li><button type="button" className={clsx(styles.colorRed)} /></li>
+              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+      </div>
+    </article>
+  )
+};
+
+export default Product;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import productsData from '../../data/products';
+import Product from '../Product/Product';
+
+const Products = () => {
+  const [products]  = useState(productsData);
+  
+  return (
+    <section>
+      {products.map(product => <Product key={product.id} {...products[0]}/>)}
+    </section>
+  );
+};
+
+export default Products;
