bc8fdd814634ee31b72da428b466e303aba8fafc
Szylman96 <szymek2255@gmail.com>
2022-07-08 18:20:35 +0200

    add useMemo to product.js

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import { useState, } from 'react';
+import { useState, useMemo } from 'react';
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from '../ProductForm/ProductForm';
 
@@ -17,12 +17,15 @@ const Product = props => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
   const [currentPrice, setCurrentPrice] = useState(props.basePrice);
-
+  const [additionalPrice, setAdditionalPrice] = useState('');
 
 
   const getPrice = (price) => {
-    return setCurrentPrice(props.basePrice + price);
+    return setAdditionalPrice(price);
   };
+  useMemo(() => {  
+    return setCurrentPrice(props.basePrice + additionalPrice);
+  }, [props.basePrice, additionalPrice]);
 
   const prepareSummary = props => {
     return console.log(
